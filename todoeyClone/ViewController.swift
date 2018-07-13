01afe9ecf9cb796dec52e147ec596c5430053c41
Andy Trinidad <andytrinidad@icloud.com>
2018-07-12 18:37:11 -0700

    Initial Commit

---
+++ todoeyClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todoeyClone
+//
+//  Created by Andy Trinidad on 7/12/18.
+//  Copyright © 2018 Andy Trinidad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
